38baf683eec17387cbbb4a9a459064a0d2745ee1
Rippa <vovan9098@mail.ru>
2020-12-25 03:01:59 +0300

    Completed task and implemented AI predictions

--- src/ru/geekbrains/lesson4/TicTacToe.java
+++ src/ru/geekbrains/lesson4/TicTacToe.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class TicTacToe {
 
     public static final int SIZE = 5;
-    public static final int DOTS_TO_WIN = 4;
+    public static final int DOTS_TO_WIN = 3;
     public static final char[][] map = new char[SIZE][SIZE];
     public static final char DOT_EMPTY = '•';
     public static final char DOT_X = 'X';
@@ -16,43 +16,53 @@ public class TicTacToe {
     public static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        fillingEmptyArray();
-        drawMap();
-
+        int replay;
         do {
-            humanTurn();
+            fillingEmptyArray();
             drawMap();
-            if (linesChecker(DOT_X)) break;
-            if (tieStatus()) break;
 
-            AITurn();
-            drawMap();
-            if (linesChecker(DOT_O)) break;
-        } while (!tieStatus());
+            do {
+                humanTurn();
+                drawMap();
+                if (linesChecker(DOT_X, false)) break;
+                if (tieStatus()) break;
 
+                AITurn();
+                drawMap();
+                if (linesChecker(DOT_O, false)) break;
+            } while (!tieStatus());
+
+            // Перезапуск игры
+
+            System.out.println("Хотите сыграть еще раз? Введите 1 что бы продолжить: ");
+            if (!SCANNER.hasNextInt()){
+                SCANNER.nextLine();
+                return;
+            }
+            replay = SCANNER.nextInt();
+        }while (replay == 1);
     }
 
     // Проверка строк и столбцов на победу
 
-    private static boolean linesChecker(char dot) {
+    private static boolean linesChecker(char dot, boolean predictionStatus) {
         int rowWinFlag = 0;
         int colWinFlag = 0;
         for (int rowIndex = 0; rowIndex < SIZE; rowIndex++) {
             for (int colIndex = 0; colIndex < SIZE; colIndex++) {
                 rowWinFlag = comboChecker(rowIndex, colIndex, rowWinFlag, dot);
                 colWinFlag = comboChecker(colIndex, rowIndex, colWinFlag, dot);
-                if (winStatus(dot, rowWinFlag, colWinFlag)) return true;
+                if (winStatus(dot, rowWinFlag, colWinFlag, predictionStatus)) return true;
             }
             colWinFlag = 0;
             rowWinFlag = 0;
         }
-        if (diagonalsChecker(dot)) return true;
-        return false;
+        return diagonalsChecker(dot, predictionStatus);
     }
 
     // Проверка диагоналей на победу
 
-    private static boolean diagonalsChecker(char dot) {
+    private static boolean diagonalsChecker(char dot, boolean predictionStatus) {
         int diagCount = 0;
         int diagCountMirrored = 0;
 
@@ -61,7 +71,7 @@ public class TicTacToe {
         for (int diagonal = 0; diagonal < SIZE; diagonal++) {
             diagCount = comboChecker(diagonal, diagonal, diagCount, dot);
             diagCountMirrored = comboChecker(diagonal, SIZE - 1 - diagonal, diagCountMirrored, dot);
-            if (winStatus(dot, diagCountMirrored, diagCount)) return true;
+            if (winStatus(dot, diagCountMirrored, diagCount, predictionStatus)) return true;
         }
         diagCount = 0;
         diagCountMirrored = 0;
@@ -72,14 +82,14 @@ public class TicTacToe {
             for (int rowIndex = 0; rowIndex < SIZE - extraDiagonals - 1; rowIndex++) {
                 diagCount = comboChecker(rowIndex + 1 + extraDiagonals, rowIndex, diagCount, dot);
                 diagCountMirrored = comboChecker(rowIndex, rowIndex + 1 + extraDiagonals, diagCountMirrored, dot);
-                if (winStatus(dot, diagCountMirrored, diagCount)) return true;
+                if (winStatus(dot, diagCountMirrored, diagCount, predictionStatus)) return true;
             }
             diagCount = 0;
             diagCountMirrored = 0;
             for (int rowIndexInverted = 0, colIndexInverted = SIZE - extraDiagonals - 2; rowIndexInverted < SIZE - extraDiagonals - 1; rowIndexInverted++, colIndexInverted--) {
                 diagCount = comboChecker(rowIndexInverted, colIndexInverted, diagCount, dot);
                 diagCountMirrored = comboChecker(rowIndexInverted + 1 + extraDiagonals, colIndexInverted + 1 + extraDiagonals, diagCountMirrored, dot);
-                if (winStatus(dot, diagCountMirrored, diagCount)) return true;
+                if (winStatus(dot, diagCountMirrored, diagCount, predictionStatus)) return true;
             }
             diagCount = 0;
             diagCountMirrored = 0;
@@ -90,8 +100,9 @@ public class TicTacToe {
 
     // Проверка n-го кол-ва символов подряд для победы
 
-    private static boolean winStatus(char dot, int rowWinFlag, int colWinFlag) {
+    private static boolean winStatus(char dot, int rowWinFlag, int colWinFlag, boolean predictionStatus) {
         if (rowWinFlag == DOTS_TO_WIN || colWinFlag == DOTS_TO_WIN) {
+            if (predictionStatus) return true;
             if (dot == DOT_X)
                 System.out.println("Вы победили!");
             if (dot == DOT_O)
@@ -119,11 +130,34 @@ public class TicTacToe {
     private static void AITurn() {
         int rowIndex;
         int colIndex;
+        if (prediction(DOT_O)) return;
+        if (prediction(DOT_X)) return;
         do {
             rowIndex = RANDOM.nextInt(SIZE);
             colIndex = RANDOM.nextInt(SIZE);
         } while (!compCheckCell(rowIndex, colIndex));
         map[rowIndex][colIndex] = DOT_O;
+    }
+
+    // Метод "угадывания" выигрышных комбинаций
+
+    private static boolean prediction(char dot) {
+        for (int rowIndex = 0; rowIndex < SIZE; rowIndex++) {
+            for (int colIndex = 0; colIndex < SIZE; colIndex++) {
+                if (compCheckCell(rowIndex, colIndex)) {
+                    map[rowIndex][colIndex] = dot;
+                    if (linesChecker(dot, true)&&dot==DOT_O)
+                        return true;
+                    else if (linesChecker(dot, true)&&dot==DOT_X) {
+                        map[rowIndex][colIndex] = DOT_O;
+                        return true;
+                    }                        
+                    else map[rowIndex][colIndex] = DOT_EMPTY;
+                }
+            }
+
+        }
+        return false;
     }
 
     // Ход игрока
@@ -133,8 +167,18 @@ public class TicTacToe {
         int colIndex = -1;
         do {
             System.out.print("Введите номер строки: ");
+            if (!SCANNER.hasNextInt()){
+                SCANNER.nextLine();
+                System.out.println("Введите число!");
+                continue;
+            }
             rowIndex = SCANNER.nextInt() - 1;
             System.out.print("Введите номер столбца: ");
+            if (!SCANNER.hasNextInt()){
+                SCANNER.nextLine();
+                System.out.println("Введите число!");
+                continue;
+            }
             colIndex = SCANNER.nextInt() - 1;
         } while (!checkCell(rowIndex, colIndex));
         map[rowIndex][colIndex] = DOT_X;
@@ -157,10 +201,7 @@ public class TicTacToe {
     // Проверка занятости ячейки для компьютера, без вывода сообщений в консоль и без проверки диапазона
 
     private static boolean compCheckCell(int rowIndex, int colIndex) {
-        if (map[rowIndex][colIndex] != DOT_EMPTY) {
-            return false;
-        }
-        return true;
+        return map[rowIndex][colIndex] == DOT_EMPTY;
     }
 
     private static int comboChecker(int rowIndex, int colIndex, int flag, int dot) {
