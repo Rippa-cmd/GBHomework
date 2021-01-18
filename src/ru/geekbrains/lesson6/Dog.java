285b9d981f93c51041d3e988f7438a255df80cb2
Rippa <vovan9098@mail.ru>
2021-01-19 00:21:35 +0300

    Extended lesson 7 - merge conflict

--- src/ru/geekbrains/lesson6/Dog.java
+++ src/ru/geekbrains/lesson6/Dog.java
@@ -1,12 +1,16 @@
 package ru.geekbrains.lesson6;
 
 public class Dog extends Animals {
+
+    private static int dogsAmount = 0;
+
     public Dog(String name) {
         super(name, 500, 0.5, 10);
-        super.incrementDogsQuantity();
+        dogsAmount++;
         System.out.println(super.toString());
     }
 
+    public static int getDogsAmount() {
+        return dogsAmount;
+    }
 }
-
-
