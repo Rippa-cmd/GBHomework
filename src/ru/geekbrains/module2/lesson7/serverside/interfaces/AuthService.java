7d661d4942e41a0574fd99ec31915dabbd4e8b2f
Rippa <vovan9098@mail.ru>
2021-03-10 01:34:31 +0300

    Module 3, lesson 2 - DB (Network chat)

--- src/ru/geekbrains/module2/lesson7/serverside/interfaces/AuthService.java
+++ src/ru/geekbrains/module2/lesson7/serverside/interfaces/AuthService.java
@@ -3,11 +3,18 @@ package ru.geekbrains.module2.lesson7.serverside.interfaces;
 public interface AuthService {
     void start();
     void stop();
+
     String getNicknameByLoginAndPassword(String login, String password);
 
     // Для проверки занятости регестрируемого ника
     boolean isNicknameBusy(String nickname);
 
+    // Для проверки занятости регестрируемого логина
+    boolean isLoginBusy(String nickname);
+
     // Добавление новой учетной записи
-    void addEntryList(String[] logPassNick);
+    boolean addEntry(String[] logPassNick);
+
+    // Заменяет ник в БД на новый
+    void nickChanger(String oldNick, String newNick);
 }
