0655d3185feb3b4267db2ea52254f3263b1273a0
Rippa-cmd <75029667+Rippa-cmd@users.noreply.github.com>
2021-03-17 16:36:10 +0300

    Revert "Module 3, lesson 2 - DB (Network chat)"
    This reverts commit f27fdb8ff1f448eac825d3ed4976b5aa173ab6b6.

--- src/ru/geekbrains/module2/lesson7/serverside/interfaces/AuthService.java
+++ src/ru/geekbrains/module2/lesson7/serverside/interfaces/AuthService.java
@@ -3,18 +3,11 @@ package ru.geekbrains.module2.lesson7.serverside.interfaces;
 public interface AuthService {
     void start();
     void stop();
-
     String getNicknameByLoginAndPassword(String login, String password);
 
     // Для проверки занятости регестрируемого ника
     boolean isNicknameBusy(String nickname);
 
-    // Для проверки занятости регестрируемого логина
-    boolean isLoginBusy(String nickname);
-
     // Добавление новой учетной записи
-    boolean addEntry(String[] logPassNick);
-
-    // Заменяет ник в БД на новый
-    void nickChanger(String oldNick, String newNick);
+    void addEntryList(String[] logPassNick);
 }
